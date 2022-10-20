83ff2e33423bd3083ade79ae8831a29c669f55b6
Brittany Davis <bddavis2011@gmail.com>
2022-10-19 20:19:41 -0500

    trying class 3 without class 2

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using SpaDay.ViewModels;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+            return View(addEventViewModel);
+        }
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User(addUserViewModel.Username, addUserViewModel.Email, addUserViewModel.Password);
+                if (addUserViewModel.Password == addUserViewModel.VerifyPassword)
+                {
+                    return View("Index", user);
+            } else
+            {
+                ViewBag.error = "Passwords do not match! Try again!";
+                return View("Add", user);
+            }
+        }
+            ViewBag.Error = "One or more inputs are not valid";
+            return View("Add");
+       
+    }
+}
---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.Models
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 15 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters.")]
+        public string Password { get; set; }
+        
+
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+    }
+
+
+}
+}
